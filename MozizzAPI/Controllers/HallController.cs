9b32d3b32d8b8a1010eeab81745ec6eeacce58c1
Roli <tothr@kkszki.hu>
2026-01-22 09:14:58 +0100

    Hibák kijavítása és HallDto törlése, mert felesleges, HallController GetAllHall végpont kész

--- MozizzAPI/Controllers/HallController.cs
+++ MozizzAPI/Controllers/HallController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MozizzAPI.Models;
 
 namespace MozizzAPI.Controllers
 {
@@ -7,5 +8,27 @@ namespace MozizzAPI.Controllers
     [ApiController]
     public class HallController : ControllerBase
     {
+        private readonly MozizzContext _context;
+
+        public HallController(MozizzContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet("GetAllHall")]
+        public IActionResult GetAllHall()
+        {
+            try
+            {
+                var termek = _context.Halls.ToList();
+                return Ok(termek);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { üzenet = "Hiba a lekérdezés során", hiba = ex.Message });
+            }
+
+        }
     }
 }
--- MozizzAPI/DTOS/HallDto.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace MozizzAPI.DTOS
-{
-    public class HallDto
-    {
-    }
-}
