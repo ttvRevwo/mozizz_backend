c031f06054b864cade23a06e20177df81fc4fc2e
Roli <tothr@kkszki.hu>
2026-01-29 10:41:06 +0100

    ShowtimeController létrehozva és GetAllShowtimes végpont elkészítve

---
+++ MozizzAPI/Controllers/ShowtimeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MozizzAPI.Models;
+
+namespace MozizzAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShowtimeController : ControllerBase
+    {
+
+        private readonly MozizzContext _context;
+
+        public ShowtimeController(MozizzContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("GetAllShowtimes")]
+        public IActionResult GetAllShowtimes()
+        {
+            try
+            {
+                var showtimes = _context.Showtimes
+                    .Include(s => s.Movie)
+                    .Include(s => s.Hall)
+                    .Select(s => new {
+                        s.ShowtimeId,
+                        MovieTitle = s.Movie.Title,
+                        HallName = s.Hall.Name,
+                        Date = s.ShowDate.ToShortDateString(),
+                        Time = s.ShowTime1.ToString()
+                    })
+                    .ToList();
+
+                return Ok(showtimes);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { hiba = ex.Message });
+            }
+        }
+    }
+}
