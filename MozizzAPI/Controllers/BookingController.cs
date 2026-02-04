d02fff37d1c5378f818ad0465902e82a0cd3ea67
Roli <tothr@kkszki.hu>
2026-02-04 09:18:07 +0100

    Elkészült a BookingDto és a hozzá tartozó Controller GetSeatsForShowtime végponttal

---
+++ MozizzAPI/Controllers/BookingController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MozizzAPI.Models;
+
+namespace MozizzAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingController : ControllerBase
+    {
+        private readonly MozizzContext _context;
+
+        public BookingController(MozizzContext context)
+        {
+            _context = context;
+        }
+
+      
+        [HttpGet("GetSeatsForShowtime")]
+        public IActionResult GetSeatsForShowtime(int showtimeId)
+        {
+            try
+            {
+               
+                var showtime = _context.Showtimes.Find(showtimeId);
+                if (showtime == null) return NotFound("A vetítés nem található.");
+
+               
+                var allSeats = _context.Seats
+                    .Where(s => s.HallId == showtime.HallId)
+                    .ToList();
+
+               
+                var reservedSeatIds = _context.Reservedseats
+                    .Where(rs => rs.Reservation.ShowtimeId == showtimeId)
+                    .Select(rs => rs.SeatId)
+                    .ToList();
+
+                var result = allSeats.Select(s => new
+                {
+                    s.SeatId,
+                    s.SeatNumber,
+                    s.IsVip,
+                    IsReserved = reservedSeatIds.Contains(s.SeatId)
+                }).ToList();
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ MozizzAPI/DTOS/BookingDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MozizzAPI.DTOS
+{
+    public class BookingDto
+    {
+        public int UserId { get; set; }
+        public int ShowtimeId { get; set; }
+        public List<int> SeatIds { get; set; } 
+    }
+}
