0c7c3ec07f591588e5ebff0028e75809f79952ea
Roli <tothr@kkszki.hu>
2026-03-04 09:39:10 +0100

    ReservationController létrehozva

---
+++ MozizzAPI/Controllers/ReservationController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MozizzAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationController : ControllerBase
+    {
+    }
+}
