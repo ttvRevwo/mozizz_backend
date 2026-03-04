c7a6437a3ed67bdd632d3e2f2b7fd593ca4e0aa3
Roli <tothr@kkszki.hu>
2026-03-04 10:03:39 +0100

    ReservationController/MyReservations végpont kész

--- MozizzAPI/Controllers/ReservationController.cs
+++ MozizzAPI/Controllers/ReservationController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MozizzAPI.Models;
 
 namespace MozizzAPI.Controllers
 {
@@ -7,5 +10,44 @@ namespace MozizzAPI.Controllers
     [ApiController]
     public class ReservationController : ControllerBase
     {
+        private readonly MozizzContext _context;
+
+        public ReservationController(MozizzContext context)
+        {
+            _context = context;
+        }
+
+        [Authorize]
+        [HttpGet("MyReservations/{userId}")]
+        public async Task<IActionResult> GetMyHistory(int userId)
+        {
+           
+            var history = await _context.Reservations
+                .Include(r => r.Showtime)
+                    .ThenInclude(s => s.Movie)
+                .Include(r => r.Reservedseats)
+                    .ThenInclude(rs => rs.Seat)
+                .Where(r => r.UserId == userId)
+                .OrderByDescending(r => r.ReservationDate) 
+                .Select(r => new {
+                    r.ReservationId,
+                    FilmCim = r.Showtime.Movie.Title,
+                    Datum = r.Showtime.ShowDate.ToShortDateString(),
+                    Idopont = r.Showtime.ShowTime1,
+                    Statusz = r.Status,
+                    Szekek = string.Join(", ", r.Reservedseats.Select(rs => rs.Seat.SeatNumber)),
+                    Vegosszeg = r.Reservedseats.Count * 2500,
+                    LefoglaltvaEkkor = r.ReservationDate
+                })
+                .ToListAsync();
+
+            if (!history.Any())
+            {
+                return NotFound(new { uzenet = "Még nincsenek foglalásaid." });
+            }
+
+            return Ok(history);
+        }
+
     }
 }
