0bbd9c766d2349fe4f54b9a1a09c8141621d324d
Tegnap <90897017+Tegnap@users.noreply.github.com>
2026-03-04 22:14:00 +0100

    EmailService létrehozva

---
+++ MozizzAPI/Services/EmailService.cs
@@ -0,0 +1,65 @@
+﻿using System.Net;
+using System.Net.Mail;
+using MozizzAPI.Models;
+
+namespace MozizzAPI.Services
+{
+    public class EmailService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly MozizzContext _context;
+
+        public EmailService(IConfiguration configuration, MozizzContext context)
+        {
+            _configuration = configuration;
+            _context = context;
+        }
+
+        public async Task SendEmailAsync(int userId, string targetEmail, string emailType, string subject, string body)
+        {
+
+            var log = new Emaillog
+            {
+                UserId = userId,
+                EmailType = emailType,
+                Subject = subject,
+                Body = body,
+                SentAt = DateTime.Now
+            };
+            try
+            {
+                var emailConfig = _configuration.GetSection("EmailSettings");
+                string senderEmail = emailConfig["Email"];
+                string appPassword = emailConfig["Password"];
+
+                using var smtp = new SmtpClient("smtp.gmail.com")
+                {
+                    Port = 587,
+                    EnableSsl = true,
+                    Credentials = new NetworkCredential(senderEmail, appPassword)
+                };
+
+                using var message = new MailMessage(senderEmail, targetEmail, subject, body) { IsBodyHtml = true };
+
+              
+                await smtp.SendMailAsync(message);
+
+                log.Status = "Sent";
+            }
+            catch (Exception ex)
+            {
+                log.Status = "Failed";
+                Console.WriteLine("Hiba az e-mail küldésekor: " + ex.Message);
+            }
+            finally
+            {
+                
+                _context.Emaillogs.Add(log);
+                await _context.SaveChangesAsync();
+            }
+
+        }
+
+
+    }
+}
