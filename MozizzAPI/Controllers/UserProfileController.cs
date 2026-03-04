33104f342ee7e7d6052b11132b36cc5d33b69139
Tegnap <90897017+Tegnap@users.noreply.github.com>
2026-03-04 17:49:51 +0100

    UserProfileController/ChangePassword végpont kész

--- MozizzAPI/Controllers/UserProfileController.cs
+++ MozizzAPI/Controllers/UserProfileController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using MozizzAPI.DTOS;
 using MozizzAPI.Models;
 
 namespace MozizzAPI.Controllers
@@ -38,6 +39,23 @@ namespace MozizzAPI.Controllers
 
             await _context.SaveChangesAsync();
             return Ok(new { uzenet = "Profil sikeresen frissítve!" });
+        }
+
+        [HttpPost("ChangePassword/{userId}")]
+        public async Task<IActionResult> ChangePassword(int userId, [FromBody] PasswordChangeDto dto)
+        {
+            var user = await _context.Users.FindAsync(userId);
+            if (user == null) return NotFound();
+
+            if (!BCrypt.Net.BCrypt.Verify(dto.OldPassword, user.PasswordHash))
+            {
+                return BadRequest("A jelenlegi jelszó nem megfelelő!");
+            }
+
+            user.PasswordHash = BCrypt.Net.BCrypt.HashPassword(dto.NewPassword);
+            await _context.SaveChangesAsync();
+
+            return Ok(new { uzenet = "Jelszó sikeresen megváltoztatva!" });
         }
     }
 }
