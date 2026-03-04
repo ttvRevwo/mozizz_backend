d5645af8b8dba0096d5526832bb61ac3d9ac9920
Roli <tothr@kkszki.hu>
2026-03-04 12:07:14 +0100

    UserProfileController létrehozva, valamint a hozzá tartozó DTO elkészítve

--- MozizzAPI/Controllers/ReservationController.cs
+++ MozizzAPI/Controllers/ReservationController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using MozizzAPI.Models;
+using Mysqlx.Crud;
 
 namespace MozizzAPI.Controllers
 {
---
+++ MozizzAPI/DTOS/PasswordChangeDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MozizzAPI.DTOS
+{
+    public class PasswordChangeDto
+    {
+        public string OldPassword { get; set; } = null!;
+        public string NewPassword { get; set; } = null!;
+    }
+}
