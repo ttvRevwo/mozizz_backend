fbc16f913453ce42bd991c0a0420a223781de343
Roli <tothr@kkszki.hu>
2026-03-13 09:34:20 +0100

    BuffetDto letrehozva

---
+++ MozizzAPI/DTOS/BuffetItemDto.cs
@@ -0,0 +1,11 @@
+﻿namespace MozizzAPI.DTOS
+{
+    public class BuffetItemDto
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public int Price { get; set; }
+        public string? Category { get; set; }
+        public bool? IsAvailable { get; set; }
+    }
+}
