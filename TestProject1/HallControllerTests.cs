8d646230748e8a62791586271f903f668789926e
Roli <tothr@kkszki.hu>
2026-04-15 09:11:31 +0200

    HallControllerTest kész GetAllHall végpont tesztelve

---
+++ TestProject1/HallControllerTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MozizzAPI.Controllers;
+using MozizzAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class HallControllerTests
+    {
+        MozizzContext _context = null!;
+        HallController _controller = null!;
+
+        private MozizzContext CreateInMemoryContext()
+        {
+            var options = new DbContextOptionsBuilder<MozizzContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+            return new MozizzContext(options);
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _context = CreateInMemoryContext();
+
+            _context.Halls.AddRange(
+                new Hall { HallId = 1, Name = "Terem 1", Location = "Fszt.", SeatingCapacity = 100 },
+                new Hall { HallId = 2, Name = "Terem 2", Location = "Emelet", SeatingCapacity = 50 }
+            );
+            _context.SaveChanges();
+            _context.ChangeTracker.Clear();
+            _controller = new HallController(_context);
+        }
+
+        [TestMethod]
+        public void GetAllHall_ReturnsOkWithList()
+        {
+            var result = _controller.GetAllHall();
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okResult = (OkObjectResult)result;
+            var halls = okResult.Value as List<Hall>;
+            Assert.IsNotNull(halls);
+            Assert.AreEqual(2, halls.Count);
+        }
+
+
+    
+        
+    }
+}
