d5cfc55b774209dce6426cad3fe701b94795e8f5
Roli <tothr@kkszki.hu>
2026-04-15 10:23:42 +0200

    UserProfileControllerTests létrehozva

---
+++ TestProject1/UserProfileControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MozizzAPI.Controllers;
+using MozizzAPI.Models;
+using MozizzAPI.DTOS;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class UserProfileControllerTests
+    {
+        private MozizzContext _context = null!;
+        private IConfiguration _config = null!;
+        private UserProfileController _controller = null!;
+     
+    }
+}
