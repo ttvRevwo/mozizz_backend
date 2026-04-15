0edeb5eb698caa07c2c6ea42a3fa54e1d3711c8e
Roli <tothr@kkszki.hu>
2026-04-15 10:21:27 +0200

    UserControllerTests összes végpontra teszt kész

--- TestProject1/UserControllerTests.cs
+++ TestProject1/UserControllerTests.cs
@@ -36,5 +36,60 @@ namespace TestProject1
 
             _controller = new UserController(_context);
         }
+
+        [TestMethod]
+        public void GetAllUsers_VisszaadjaAzOsszesFelhasznalot()
+        {
+            var result = _controller.GetAllUsers();
+
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okResult = (OkObjectResult)result;
+            var users = okResult.Value as List<User>;
+
+            Assert.IsNotNull(users);
+            Assert.AreEqual(2, users.Count);
+        }
+
+        [TestMethod]
+        public void GetUserById_LetezoUser_OkEredmenytAd()
+        {
+            var result = _controller.GetUserById(1);
+
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okResult = (OkObjectResult)result;
+            var user = okResult.Value as User;
+
+            Assert.IsNotNull(user);
+            Assert.AreEqual("Admin User", user.Name);
+        }
+
+        [TestMethod]
+        public void GetUserById_NemLetezoUser_NotFoundotAd()
+        {
+           var result = _controller.GetUserById(99);
+
+            Assert.IsInstanceOfType(result, typeof(NotFoundObjectResult));
+        }
+
+        [TestMethod]
+        public void DeleteUser_LetezoUser_TorliAzAdatbazisbol()
+        {
+            var result = _controller.DeleteUser(2);
+
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            Assert.AreEqual(1, _context.Users.Count());
+        }
+
+        [TestMethod]
+        public void CleanupCodes_CsakALejartKodokatTorli()
+        {
+            var result = _controller.CleanupCodes();
+
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            Assert.AreEqual(1, _context.UserVerifications.Count());
+
+            var megmaradtKod = _context.UserVerifications.First();
+            Assert.AreEqual(2, megmaradtKod.Id);
+        }
     }
 }
