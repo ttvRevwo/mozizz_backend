697d32dbd9ad01b8f8cd64c22e20f00d390b4007
Roli <tothr@kkszki.hu>
2026-04-15 10:04:55 +0200

    ReservationControllerTests létrehozva

---
+++ TestProject1/ReservationControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MozizzAPI.Controllers;
+using MozizzAPI.Models;
+using MozizzAPI.Services;
+
+
+namespace TestProject1
+{
+    [TestClass]
+    public class ReservationControllerTests
+    {
+        private MozizzContext _context = null!;
+        private IConfiguration _config = null!;
+        private ReservationController _controller = null!;
+    }
+}
