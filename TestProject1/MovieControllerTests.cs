c8aabe9312c814d610789998493b2bb61b6f6fa3
Roli <tothr@kkszki.hu>
2026-04-15 09:22:57 +0200

    MovieControllerTests létrehozva

---
+++ TestProject1/MovieControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using MozizzAPI.Controllers;
+using MozizzAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class MovieControllerTests
+    {
+        MozizzContext _context = null!;
+        MovieController _controller = null!;
+    }
+}
