726327550b89ca37c8f2f2d8841209a98aebcf0f
Roli <tothr@kkszki.hu>
2026-04-15 09:26:31 +0200

    ShowtimeControllerTests létrehozva

---
+++ TestProject1/ShowtimeControllerTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using MozizzAPI.Controllers;
+using MozizzAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class ShowtimeControllerTests
+    {
+        MozizzContext _context = null!;
+        ShowtimeController _controller = null!;
+    }
+    }
