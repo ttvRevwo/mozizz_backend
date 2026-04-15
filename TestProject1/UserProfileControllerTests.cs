57c77c1d4053be0bb4930902105f8f544ebd244a
Roli <tothr@kkszki.hu>
2026-04-15 10:24:31 +0200

    UserProfileControllerTests InMemory kész

--- TestProject1/UserProfileControllerTests.cs
+++ TestProject1/UserProfileControllerTests.cs
@@ -17,6 +17,35 @@ namespace TestProject1
         private MozizzContext _context = null!;
         private IConfiguration _config = null!;
         private UserProfileController _controller = null!;
-     
+
+        [TestInitialize]
+        public void Setup()
+        {
+            var options = new DbContextOptionsBuilder<MozizzContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+            _context = new MozizzContext(options);
+
+            _config = new ConfigurationBuilder()
+                .AddInMemoryCollection(new Dictionary<string, string?>
+                {
+                    { "EmailSettings:Email", "teszt@teszt.hu" },
+                    { "EmailSettings:Password", "kamujelszo" }
+                }).Build();
+
+            _context.Users.Add(new User
+            {
+                UserId = 1,
+                Name = "Eredeti Név",
+                Email = "teszt@teszt.hu",
+                Phone = "+36301234567",
+                PasswordHash = BCrypt.Net.BCrypt.HashPassword("regiJelszo123"),
+                CreatedAt = DateTime.Now
+            });
+
+            _context.SaveChanges();
+
+            _controller = new UserProfileController(_context, _config);
+        }
     }
 }
