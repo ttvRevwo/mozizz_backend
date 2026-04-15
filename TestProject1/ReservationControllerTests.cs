e90431f3881dac83687342ab4df9455ddf4fdeeb
Roli <tothr@kkszki.hu>
2026-04-15 10:08:06 +0200

    ReservationControllerTests InMemory megcsinálva

--- TestProject1/ReservationControllerTests.cs
+++ TestProject1/ReservationControllerTests.cs
@@ -19,5 +19,56 @@ namespace TestProject1
         private MozizzContext _context = null!;
         private IConfiguration _config = null!;
         private ReservationController _controller = null!;
+        [TestInitialize]
+        public void Setup()
+        {
+            var options = new DbContextOptionsBuilder<MozizzContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+            _context = new MozizzContext(options);
+
+            _config = new ConfigurationBuilder()
+                .AddInMemoryCollection(new Dictionary<string, string?>
+                {
+                    { "EmailSettings:Email", "teszt@teszt.hu" },
+                    { "EmailSettings:Password", "kamujelszo" }
+                }).Build();
+
+            _context.Users.Add(new User { UserId = 1, Name = "Teszt User", Email = "teszt@teszt.hu" });
+            _context.Movies.Add(new Movie { MovieId = 1, Title = "Teszt Film" });
+            _context.Halls.Add(new Hall { HallId = 1, Name = "Teszt Terem" });
+            _context.Seats.Add(new Seat { SeatId = 1, HallId = 1, SeatNumber = "A1" });
+
+            _context.Showtimes.AddRange(
+                new Showtime
+                {
+                    ShowtimeId = 1,
+                    MovieId = 1,
+                    HallId = 1,
+                    ShowDate = DateTime.Today.AddDays(2),
+                    ShowTime1 = new TimeSpan(18, 0, 0)
+                },
+                new Showtime
+                {
+                    ShowtimeId = 2,
+                    MovieId = 1,
+                    HallId = 1,
+                    ShowDate = DateTime.Today,
+                    ShowTime1 = DateTime.Now.AddHours(1).TimeOfDay
+                }
+            );
+
+            _context.Reservations.AddRange(
+                new Reservation { ReservationId = 1, UserId = 1, ShowtimeId = 1, ReservationDate = DateTime.Now, Status = "Confirmed" },
+                new Reservation { ReservationId = 2, UserId = 1, ShowtimeId = 2, ReservationDate = DateTime.Now, Status = "Confirmed" }
+            );
+
+            _context.Reservedseats.Add(new Reservedseat { ReservedSeatId = 1, ReservationId = 1, SeatId = 1 });
+
+            _context.SaveChanges();
+
+            var fakeEmailService = new EmailService(_config, _context);
+            _controller = new ReservationController(_context, _config, fakeEmailService);
+        }
     }
 }
