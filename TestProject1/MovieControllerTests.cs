b16ebd78ad0021d5c7891c67c7bf1ef625cdabdc
Roli <tothr@kkszki.hu>
2026-04-15 09:25:34 +0200

    MovieController/Get végpontok teszt kész

--- TestProject1/MovieControllerTests.cs
+++ TestProject1/MovieControllerTests.cs
@@ -64,5 +64,59 @@ namespace TestProject1
             _context.ChangeTracker.Clear();
             _controller = new MovieController(_context, CreateFakeConfig());
         }
+
+
+        [TestMethod]
+        public void GetAllMovies_ReturnsOkWithList()
+        {
+            var result = _controller.GetAllMovies();
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okResult = (OkObjectResult)result;
+            var filmek = okResult.Value as List<Movie>;
+            Assert.IsNotNull(filmek);
+            Assert.AreEqual(2, filmek.Count);
+        }
+
+        [TestMethod]
+        public void GetAllMovies_UresAdatbazis_ReturnsUresLista()
+        {
+            _context.Movies.RemoveRange(_context.Movies);
+            _context.SaveChanges();
+
+            var result = _controller.GetAllMovies();
+
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var filmek = ((OkObjectResult)result).Value as List<Movie>;
+            Assert.IsNotNull(filmek);
+            Assert.AreEqual(0, filmek.Count);
+        }
+
+
+
+        [TestMethod]
+        public void GetMovieById_LetezoId_ReturnsOk()
+        {
+            var result = _controller.GetMovieById(1);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var film = ((OkObjectResult)result).Value as Movie;
+            Assert.IsNotNull(film);
+            Assert.AreEqual("Inception", film.Title);
+            Assert.AreEqual("Sci-Fi", film.Genre);
+        }
+
+        [TestMethod]
+        public void GetMovieById_NemLetezoId_ReturnsNotFound()
+        {
+            var result = _controller.GetMovieById(999);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(NotFoundObjectResult));
+            var notFound = (NotFoundObjectResult)result;
+            Assert.AreEqual("A kért film nem található.", notFound.Value);
+        }
     }
 }
