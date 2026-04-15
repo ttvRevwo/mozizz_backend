9b7527895ff257fd8858a87b2edfeef10886dc2f
Roli <tothr@kkszki.hu>
2026-04-15 09:55:49 +0200

    BuffetControllerTests létrehozva

---
+++ TestProject1/BuffetControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MozizzAPI.Controllers;
+using MozizzAPI.Models;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class BuffetControllerTests
+    {
+        private MozizzContext _context = null!;
+        private BuffetController _controller = null!;
+    }
+}
