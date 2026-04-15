97f086c5221943d6f20ad57a2cee20913ba641ba
Roli <tothr@kkszki.hu>
2026-04-15 09:49:28 +0200

    AuthControllerTests létrehozva

---
+++ TestProject1/AuthControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MozizzAPI.Controllers;
+using MozizzAPI.Models;
+using MozizzAPI.DTOS;
+namespace TestProject1
+{
+    [TestClass]
+    public class AuthControllerTests
+    {
+        private MozizzContext _context = null!;
+        private AuthController _controller = null!;
+    }
+}
