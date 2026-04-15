bcdbbd6d3019b1b542b891c3678662cb5f3b58cc
Roli <tothr@kkszki.hu>
2026-04-15 09:34:28 +0200

    TicketControllerTests InMemory megcsinálva

--- TestProject1/TicketControllerTests.cs
+++ TestProject1/TicketControllerTests.cs
@@ -13,5 +13,100 @@ namespace TestProject1
     {
         MozizzContext _context = null!;
         TicketController _controller = null!;
+        private MozizzContext CreateInMemoryContext()
+        {
+            var options = new DbContextOptionsBuilder<MozizzContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+            return new MozizzContext(options);
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _context = CreateInMemoryContext();
+
+            _context.Userroles.Add(new Userrole { RoleId = 1, RoleName = "User" });
+            _context.Users.Add(new User
+            {
+                UserId = 1,
+                Name = "Teszt Felhasználó",
+                Email = "teszt@teszt.hu",
+                PasswordHash = "hash",
+                RoleId = 1,
+                CreatedAt = DateTime.Now
+            });
+
+            _context.Movies.Add(new Movie
+            {
+                MovieId = 1,
+                Title = "Inception",
+                Duration = 148,
+                CreatedAt = DateTime.Now
+            });
+            _context.Halls.Add(new Hall
+            {
+                HallId = 1,
+                Name = "Terem 1",
+                Location = "Fszt.",
+                SeatingCapacity = 100
+            });
+
+            _context.Seats.Add(new Seat
+            {
+                SeatId = 1,
+                HallId = 1,
+                SeatNumber = "A1",
+                IsVip = false
+            });
+
+            _context.Showtimes.Add(new Showtime
+            {
+                ShowtimeId = 1,
+                MovieId = 1,
+                HallId = 1,
+                ShowDate = DateTime.Today.AddDays(1),
+                ShowTime1 = new TimeSpan(18, 0, 0),
+                CreatedAt = DateTime.Now
+            });
+
+            _context.Reservations.Add(new Reservation
+            {
+                ReservationId = 1,
+                UserId = 1,
+                ShowtimeId = 1,
+                ReservationDate = DateTime.Now,
+                Status = "confirmed"
+            });
+
+            _context.Reservedseats.Add(new Reservedseat
+            {
+                ReservedSeatId = 1,
+                ReservationId = 1,
+                SeatId = 1
+            });
+
+            _context.Tickets.Add(new Ticket
+            {
+                TicketId = 1,
+                ReservationId = 1,
+                TicketCode = "TESZT-KOD-001",
+                IssuedDate = DateTime.Now,
+                IsUsed = false
+            });
+
+            _context.Tickets.Add(new Ticket
+            {
+                TicketId = 2,
+                ReservationId = 1,
+                TicketCode = "HASZNALT-KOD-002",
+                IssuedDate = DateTime.Now,
+                IsUsed = true
+            });
+
+            _context.SaveChanges();
+            _context.ChangeTracker.Clear();
+            _controller = new TicketController(_context);
+        }
     }
 }
