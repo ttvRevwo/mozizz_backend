c2b31e198af273d377a0cdf0750b7fd3091c3df5
Roli <tothr@kkszki.hu>
2026-04-15 09:33:26 +0200

    TicketControllerTests létrehozva

---
+++ TestProject1/TicketControllerTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using MozizzAPI.Controllers;
+using MozizzAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class TicketControllerTests
+    {
+        MozizzContext _context = null!;
+        TicketController _controller = null!;
+    }
+}
