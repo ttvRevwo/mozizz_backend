376f8cf3e37131353bfb4888d9615c2c7f0e548e
Roli <tothr@kkszki.hu>
2026-04-15 09:50:17 +0200

    AuthControllerTests InMemory megcsinálva

--- TestProject1/AuthControllerTests.cs
+++ TestProject1/AuthControllerTests.cs
@@ -14,5 +14,36 @@ namespace TestProject1
     {
         private MozizzContext _context = null!;
         private AuthController _controller = null!;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            var options = new DbContextOptionsBuilder<MozizzContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+            _context = new MozizzContext(options);
+
+            var config = new ConfigurationBuilder()
+                .AddInMemoryCollection(new Dictionary<string, string?>
+                {
+                    { "Jwt:Key", "nagyon_hosszu_titkos_kulcs_ami_legalabb_32_karakter" },
+                    { "Jwt:Issuer", "TestIssuer" },
+                    { "Jwt:Audience", "TestAudience" }
+                }).Build();
+
+            var user = new User
+            {
+                UserId = 1,
+                Name = "Teszt Elek",
+                Email = "teszt@teszt.hu",
+                PasswordHash = BCrypt.Net.BCrypt.HashPassword("titkosJelszo123"),
+                Role = new Userrole { RoleId = 1, RoleName = "User" }
+            };
+
+            _context.Users.Add(user);
+            _context.SaveChanges();
+
+            _controller = new AuthController(_context, config);
+        }
     }
 }
