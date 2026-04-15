3506d5903235b1af3fb4a206c0a72830e9df2ad1
Roli <tothr@kkszki.hu>
2026-04-15 09:58:38 +0200

    BuffetControllerTests InMemory megcsinálva

--- TestProject1/BuffetControllerTests.cs
+++ TestProject1/BuffetControllerTests.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MozizzAPI.Controllers;
 using MozizzAPI.Models;
@@ -14,5 +16,30 @@ namespace TestProject1
     {
         private MozizzContext _context = null!;
         private BuffetController _controller = null!;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            var options = new DbContextOptionsBuilder<MozizzContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+            _context = new MozizzContext(options);
+
+            var config = new ConfigurationBuilder()
+                .AddInMemoryCollection(new Dictionary<string, string?>
+                {
+                    { "CloudinarySettings:CloudName", "kamu_cloud" },
+                    { "CloudinarySettings:ApiKey", "12345" },
+                    { "CloudinarySettings:ApiSecret", "titkos_secret" }
+                }).Build();
+
+            _context.BuffetItems.AddRange(
+                new BuffetItem { ItemId = 1, Name = "Sós Popcorn", Price = 1500, IsAvailable = true, Category = "Snack" },
+                new BuffetItem { ItemId = 2, Name = "Elfogyott Kóla", Price = 800, IsAvailable = false, Category = "Ital" }
+            );
+            _context.SaveChanges();
+
+            _controller = new BuffetController(_context, config);
+        }
     }
 }
